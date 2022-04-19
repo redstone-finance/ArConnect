7ba0ceef2d95ba1e3def1945a6e5508bfab62350
asiaziola <ziola.jm@gmail.com>
2022-04-19 12:51:19 +0200

    fix: removing unnecessary script from the manifest

--- src/views/Popup/routes/FakeReporting.tsx
+++ src/views/Popup/routes/FakeReporting.tsx
@@ -359,7 +359,7 @@ export default function FakeReporting({
                 }
                 target="_blank"
               >
-                SonAR
+                SonAR.
               </a>
             </div>
           </div>
